ed3684ec376e2a0a49f0ad7a20038c9bfe4dcb0d
intelliseawolf <intelliseawolf103@gmail.com>
2024-08-14 05:27:19 -0400

    added: mobile header

--- src/components/Layout/MobileMenu.js
+++ src/components/Layout/MobileMenu.js
@@ -1,114 +1,67 @@
+import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
+
+import HamburgerIcon from "../Icon/hamburger";
+import CrossIcon from "../Icon/cross";
 
 const RouteLinks = [
   { href: "/", name: "Home" },
   { href: "/about", name: "About" },
   { href: "/products", name: "Products" },
-  { href: "/technology", name: "Technology" },
   { href: "/careers", name: "Career" },
   { href: "/contact", name: "Contact" },
 ];
 
-const MobileMenu = ({ setShowMobileMenu }) => {
+const MobileMenu = () => {
+  const [show, setShow] = useState(false);
   const location = useLocation();
 
-  return (
-    <div className="mobile-menu">
-      <div className="menu-backdrop"></div>
-      <div
-        className="close-btn"
-        onClick={() => {
-          setShowMobileMenu(false);
-        }}
-      >
-        <i className="icon-fa-times"></i>
-      </div>
+  useEffect(() => {
+    setShow(false);
+  }, [location.pathname]);
 
-      <nav className="menu-box mCustomScrollbar _mCS_1 mCS_no_scrollbar">
-        <div
-          id="mCSB_1"
-          className="mCustomScrollBox mCS-light mCSB_vertical mCSB_inside"
-          style={{ maxHeight: "822px" }}
-          tabIndex="0"
-        >
-          <div
-            id="mCSB_1_container"
-            className="mCSB_container mCS_y_hidden mCS_no_scrollbar_y"
-            style={{ position: "relative", top: "0", left: "0" }}
-            dir="ltr"
-          >
-            <div className="nav-logo">
-              <a href="/">
-                <img
-                  src="assets/logo.png"
-                  alt="logo"
-                  className="mCS_img_loaded"
-                  style={{ height: "36px" }}
-                />
+  return (
+    <div className="corzo-mobile-header-wrap">
+      <div
+        className="corzo-mobile-header corzo-header-background corzo-style-slide corzo-sticky-mobile-navigation "
+        id="corzo-mobile-header"
+      >
+        <div className="corzo-mobile-header-container corzo-container clearfix">
+          <div className="corzo-logo corzo-item-pdlr">
+            <div className="corzo-logo-inner">
+              <a className="corzo-orig-logo" href="/">
+                <img src="assets/logo.png" alt="" width="60" />
               </a>
             </div>
-            <div className="menu-outer">
-              <div
-                className="collapse navbar-collapse show clearfix"
-                id="navbarSupportedContent"
-              >
-                <ul className="navigation clearfix home-menu">
-                  {RouteLinks.map((routeLink) => (
-                    <li
-                      key={routeLink.href}
-                      className={
-                        routeLink.href === location.pathname ? "current" : ""
-                      }
-                      onClick={() => {
-                        setShowMobileMenu(false);
-                      }}
-                    >
-                      <Link to={routeLink.href}>{routeLink.name}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-            <div className="contact-info">
-              <h4>Contact Info</h4>
-              <ul>
-                <li>111 NE 1st, Miami, Florida</li>
-                <li>
-                  <a href="tel:+17185099729">+1 (718) 509 9729</a>
-                </li>
-                <li>
-                  <a href="mailto:shane@apexic.co">shane@apexic.co</a>
-                </li>
-              </ul>
-            </div>
           </div>
-          <div
-            id="mCSB_1_scrollbar_vertical"
-            className="mCSB_scrollTools mCSB_1_scrollbar mCS-light mCSB_scrollTools_vertical"
-            style={{ display: "none" }}
-          >
-            <div className="mCSB_draggerContainer">
-              <div
-                id="mCSB_1_dragger_vertical"
-                className="mCSB_dragger"
-                style={{
-                  position: "absolute",
-                  minHeight: "0px",
-                  height: "0px",
-                  top: "0px",
-                }}
-                // onContextMenu="return false;"
-              >
-                <div
-                  className="mCSB_dragger_bar"
-                  style={{ lineHeight: "0px" }}
-                ></div>
-              </div>
-              <div className="mCSB_draggerRail"></div>
+          <div className="corzo-mobile-menu-right">
+            <div
+              className="corzo-overlay-menu corzo-mobile-menu"
+              onClick={() => setShow(!show)}
+            >
+              {show ? <CrossIcon /> : <HamburgerIcon />}
             </div>
           </div>
         </div>
-      </nav>
+
+        <div
+          className="corzo-mobile-header-container corzo-container clearfix"
+          style={{ animationDuration: "1s" }}
+        >
+          {RouteLinks.map((route) => (
+            <div
+              className={`menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-12780 current_page_item current-menu-ancestor current-menu-parent current_page_parent current_page_ancestor menu-item-has-children menu-item-13227 corzo-normal-menu text-center ${
+                show ? "menu-item-show" : "menu-item-hide"
+              }`}
+              key={route.name}
+            >
+              <Link to={route.href} className="sf-with-ul-pre sf-with-ul">
+                {route.name}
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,25 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import TopHeader from "./TopHeader";
 import LowerHeader from "./LowerHeader";
-import StickyHeader from "./StickyHeader";
 import MobileMenu from "./MobileMenu";
 
 import MainFooter from "./MainFooter";
 
 const Layout = ({ children }) => {
-  const [showMobileMenu, setShowMobileMenu] = useState(false);
-
   return (
-    <div
-      className={
-        showMobileMenu ? "mobile-menu-visible boxed_wrapper" : "boxed_wrapper"
-      }
-    >
+    <div>
       <TopHeader />
-      <LowerHeader setShowMobileMenu={setShowMobileMenu} />
-      {/* <StickyHeader />
-      <MobileMenu setShowMobileMenu={setShowMobileMenu} /> */}
+      <LowerHeader />
+      <MobileMenu />
       {children}
       <MainFooter />
     </div>
